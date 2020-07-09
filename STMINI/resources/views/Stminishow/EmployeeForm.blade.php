f633f8ccf3400c4b7596287cf108b2c1a4a7154c
bobeiei13155 <bobeiei13155@gmail.com>
2020-07-09 13:44:23 +0700

    First push!

---
+++ STMINI/resources/views/Stminishow/EmployeeForm.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.stmininav')
+@section('body')
+<div class="table-responsive">
+    <h2>เพิ่มพนักงาน</h2>
+    <form action="" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Product Name">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <input type="text" class="form-control" name="description" id="description" placeholder="Description">
+        </div>
+        <div class="form-group">
+            <label for="type">Category</label>
+            <select class="form-control" name="category">
+                    <option value="men">Men</option>
+                    <option value="women">WoMen</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="type">Price</label>
+            <input type="text" class="form-control" name="price" id="price" placeholder="Price" required>
+        </div>
+        <button type="submit" name="submit" class="btn btn-success">Submit</button>
+    </form>
+</div>
+@endsection
---
+++ STMINI/resources/views/layouts/stmininav.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>STMINI</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="{{asset('/css/sidebar.css')}}" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark my-md-0 mr-md-3">
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">STMINI</h5>
+      <a class="p-2 text-dark" href="#">Home</a>
+      <a class="p-2 text-dark" href="/Stminishow/createEmployee">ข้อมูลพนักงาน</a>
+      <a class="p-2 text-dark" href="/Stminishow/createPosition">ตำแหน่ง</a>
+    </nav>
+  </div>
+  <div class="d-flex" id="wrapper">
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">Overview</div>
+      <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Insert</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Edit</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">User</a>
+      </div>
+    </div>
+    <div id="page-content-wrapper">
+      <div class="container-fluid">
+        @yield('body')
+    </div>
+  </div>
+</body>
+</html>
