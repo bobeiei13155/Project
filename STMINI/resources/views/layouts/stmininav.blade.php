68e5e0636c99786d358a2b99f6b5695184dc8fbb
bobeiei13155 <bobeiei13155@gmail.com>
2020-09-04 23:57:02 +0700

    commit

--- STMINI/resources/views/layouts/stmininav.blade.php
+++ STMINI/resources/views/layouts/stmininav.blade.php
@@ -62,3 +62,139 @@
 </body>
 
 </html>
+
+<body>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Admin Panel</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+      <a class="p-2 text-dark" href="#">Home</a>
+      <a class="p-2 text-dark" href="#">Dashboard</a>
+      <a class="p-2 text-dark" href="#">Profile</a>
+      <a class="p-2 text-dark" href="#">Help</a>
+    </nav>
+  </div>
+  <div class="d-flex" id="wrapper">
+    <div class="bg-light border-right"style="background-color: blue" id="sidebar-wrapper">
+      <div class="list-group list-group-flush">
+     
+          <!-- ///////////////////////////////////////// -->
+         
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลพนักงาน
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+        
+          <!-- ///////////////////////////////////////// -->
+          
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            ข้อมูลสินค้า
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+          
+          <!-- ///////////////////////////////////////// -->
+
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลบริษัทคู่ค้า
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+
+          <!-- ///////////////////////////////////////// -->
+         
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลลูกค้า
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+          </div>
+          <!-- ///////////////////////////////////////// -->
+
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลโปรโมชั่น
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+
+          <!-- ///////////////////////////////////////// -->
+   
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลการสั่งซื้อ
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+      
+          <!-- ///////////////////////////////////////// -->
+   
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลของแถม
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+          
+          <!-- ///////////////////////////////////////// -->
+        
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลรับสินค้า
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+    
+          <!-- ///////////////////////////////////////// -->
+
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลขายสินค้า
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+          
+          <!-- ///////////////////////////////////////// -->
+          
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลเคลมสินค้า
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+      
+          <!-- ///////////////////////////////////////// -->
+         
+            <button class="dropdown list-group-item btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ข้อมูลออกรายงาน
+            </button>
+            <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/Stminishow/showEmployee">Employee</a>
+              <a class="dropdown-item" href="/Stminishow/createPosition">Position</a>
+            </div>
+         </div>
+         
+      </div>
+    </div>
+    <div id="page-content-wrapper">
+      <div class="container-fluid">
+        @yield('body')
+      </div>
+    </div>
+</body>
+</div>
+</html>
